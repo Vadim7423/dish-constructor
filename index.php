d207fb5c1f3bc55d10c1ab94533b6f8d2921b925
Vadim7423 <vadalm@mail.ru>
2023-03-26 13:15:13 +0500

    update 1

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
-//declare(strict_types=1);
+
+const KEY = 0;
+const VALUE = 1;
 
 require 'app.php';
 $config = require 'config.php';
 
-App::Init($config)->Print();
+$url = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$params =  explode("=", parse_url($url, PHP_URL_QUERY));
+
+if(isset($params[VALUE])) {
+    App::Init($config)->FillIngredientData($params[VALUE])->Print();
+} else {
+    $result["answer"] = false;
+    $result["message"] = "empty query";
+    print_r(json_encode($result));
+}
